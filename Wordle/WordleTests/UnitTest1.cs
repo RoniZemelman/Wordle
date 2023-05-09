f10176f63a69ce1c2e876a276ae3133fcb12a387
roniz <roniz@gingersoftware.com>
2023-05-09 14:07:49 +0300

    sanity check tests - variable hits/partial

--- Wordle/WordleTests/UnitTest1.cs
+++ Wordle/WordleTests/UnitTest1.cs
@@ -104,15 +104,100 @@ namespace WordleTests
 
             Assert.AreEqual(0, guessResult.GetNumPartialMatches());
         }
-        
+
+        /*********************************************** SANITY CHECK TESTS ******************************************/
+
+        [Test]
+        [TestCase("bxxxo", 2)]
+        [TestCase("bxnxx", 2)]
+        [TestCase("xixxo", 2)]
+        [TestCase("bxxgo", 3)]
+        [TestCase("xinxo", 3)]
+        [TestCase("bixgx", 3)]
+        [TestCase("bxngo", 4)]
+        [TestCase("bixgo", 4)]
+        [TestCase("binxo", 4)]
+        public void AnalyzeGuess_UserHasVariableExactMatches_ReturnsCorrectResult(string userGuess, int expectedResult)
+        {
+            string answer = "bingo";
+
+            // var guessValidator = MockRepos
+
+            var analyzer = new GuessAnalyzer(answer);
+            var guessResult = analyzer.Analyze(userGuess);
+
+            Assert.That(guessResult.GetNumExactMatches(), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("xoxix",2)]
+        [TestCase("gxxxb", 2)]
+        [TestCase("xgxix",2)]
+        [TestCase("xxgon", 3)]
+        [TestCase("ibxox", 3)]
+        [TestCase("nxgxi", 3)]
+        [TestCase("obinx", 4)]
+        [TestCase("xnboi", 4)]
+        [TestCase("onxbi", 4)]
+        [TestCase("ibgon", 5)]
+        [TestCase("ingob", 5)]
+        [TestCase("ibgon", 5)]
+        public void AnalyzeGuess_UserHasVariablePartialMatches_ReturnsCorrectResult(string userGuess, int expectedResult)
+        {
+            string answer = "bingo";
+
+            // var guessValidator = MockRepos
+
+            var analyzer = new GuessAnalyzer(answer);
+            var guessResult = analyzer.Analyze(userGuess);
+
+            Assert.That(guessResult.GetNumPartialMatches(), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void AnalyzeGuess_UserGuessHas1Exact1Partial_ReturnsCorrectResult()
+        {
+            string userGuess = "bxxxi";
+            string answer = "bingo";
+
+            // var guessValidator = MockRepos
+
+            var analyzer = new GuessAnalyzer(answer);
+            var guessResult = analyzer.Analyze(userGuess);
+
+            Assert.AreEqual(1, guessResult.GetNumExactMatches());
+            Assert.AreEqual(1, guessResult.GetNumPartialMatches());
+        }
+
+        /*********************************************** END SANITY CHECK TESTS ******************************************/
+
+
+
+
+        //// Test Partial and Exact Matches - too early for this edge case test?
+        //[Test]
+        //public void AnalyzeGuess_UserGuessesSameLetterTwice_ReturnsOneExactMatchZeroPartialMatches()
+        //{
+        //    string answer = "sweat";
+
+        //    string userGuess = "xxeex";
+
+        //    //var guessValidator = MockRepos
+        //    var analyzer = new GuessAnalyzer(answer);
+
+        //    var guessResult = analyzer.Analyze(userGuess);
+
+        //    Assert.AreEqual(1, guessResult.GetNumExactMatches());
+        //    Assert.AreEqual(0, guessResult.GetNumPartialMatches());
+        //}
 
 
         // one partial match positions?
         // Integration test of partial and exact matches 
-        // Add Edge Case: answer "sweat" and guess "xxeex" - the first e is 1 exact, 0 partial
+        // Add Edge Case: answer "sweat" and guess "xxeex" - 1 exact, 0 partial
     }
-    
-    
+
+
 
 
 }
