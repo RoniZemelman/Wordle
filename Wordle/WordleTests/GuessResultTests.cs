ebd49d14b114c9f1e1271e2057b67654fcd332e3
roniz <roniz@gingersoftware.com>
2023-05-21 10:31:13 +0300

    GuessResultTests refactored

--- Wordle/WordleTests/GuessResultTests.cs
+++ Wordle/WordleTests/GuessResultTests.cs
@@ -6,39 +6,31 @@ namespace WordleTests
 {
     class GuessResultTests
     {
-        [Test]
-        public void GuessResult_GuessHasExactMatch_CorrectLocationIsExactMatch()
-        {
-            // Arrange
-            string userGuess = "bxxxi";
-            string answer = "bingo";
+        static string answer = "bingo";
 
+        private static GuessResult ArrangeAndAnalyze(string userGuess)
+        {
+            
             var mockWordValidator = MockRepository.GenerateStub<IWordValidator>();
             mockWordValidator.Stub(v => v.Validate(userGuess)).Return(true);
 
             var analyzer = new GuessAnalyzer(answer);
 
-            // Act
-            var guessResult = analyzer.Analyze(userGuess);
+            return analyzer.Analyze(userGuess);
+        }
 
-            // Assert
+        [Test]
+        public void GuessResult_GuessHasExactMatch_CorrectLocationIsExactMatch()
+        {
+            var guessResult = ArrangeAndAnalyze(userGuess: "bxxxi");
+
             Assert.IsTrue(guessResult.At(0).IsExactMatch());
         }
 
         [Test]
         public void GuessResult_GuessHasAtLeast1Miss_CorrectLocationIsMissed()
         {
-            // Arrange
-            string userGuess = "bxngo";
-            string answer = "bingo";
-
-            var mockWordValidator = MockRepository.GenerateStub<IWordValidator>();
-            mockWordValidator.Stub(v => v.Validate(userGuess)).Return(true);
-
-            var analyzer = new GuessAnalyzer(answer);
-
-            // Act
-            var guessResult = analyzer.Analyze(userGuess);
+            var guessResult = ArrangeAndAnalyze(userGuess: "bxngo");
 
             // Assert
             Assert.IsTrue(guessResult.At(1).Missed());
@@ -47,38 +39,17 @@ namespace WordleTests
         [Test]
         public void GuessResult_GuessHasAtLeast1PartialMatch_CorrectLocationIsPartialMatch()
         {
-            // Arrange
-            string userGuess = "xxxxi";
-            string answer = "bingo";
+            var guessResult = ArrangeAndAnalyze(userGuess: "xxxxi");
 
-            var mockWordValidator = MockRepository.GenerateStub<IWordValidator>();
-            mockWordValidator.Stub(v => v.Validate(userGuess)).Return(true);
-
-            var analyzer = new GuessAnalyzer(answer);
-
-            // Act
-            var guessResult = analyzer.Analyze(userGuess);
-
-            // Assert
             Assert.IsTrue(guessResult.At(4).IsPartialMatch());
         }
 
         [Test]  
         public void GuessResult_GuessHasAnExactMatch_LocationOfExactMatchIsNotPartialMatch()
         {
-            // Arrange
-            string userGuess = "bxxxx";
-            string answer = "bingo";
+            
+            var guessResult = ArrangeAndAnalyze(userGuess: "bxxxx");
 
-            var mockWordValidator = MockRepository.GenerateStub<IWordValidator>();
-            mockWordValidator.Stub(v => v.Validate(userGuess)).Return(true);
-
-            var analyzer = new GuessAnalyzer(answer);
-
-            // Act
-            var guessResult = analyzer.Analyze(userGuess);
-
-            // Assert 
             Assert.IsTrue(guessResult.At(0).IsExactMatch() == true
                             && guessResult.At(0).IsPartialMatch() == false);
         }
@@ -87,19 +58,9 @@ namespace WordleTests
         [Test] // Not sure this test is logically neccessary
         public void GuessResult_GuessHasAPartialMatch_LocationOfPartialMatchIsNotExactMatch()
         {
-            // Arrange
-            string userGuess = "xgxxx";
-            string answer = "bingo";
+            
+            var guessResult = ArrangeAndAnalyze(userGuess: "xgxxi");
 
-            var mockWordValidator = MockRepository.GenerateStub<IWordValidator>();
-            mockWordValidator.Stub(v => v.Validate(userGuess)).Return(true);
-
-            var analyzer = new GuessAnalyzer(answer);
-
-            // Act
-            var guessResult = analyzer.Analyze(userGuess);
-
-            // Assert 
             Assert.IsTrue(guessResult.At(1).IsPartialMatch() == true
                             && guessResult.At(1).IsExactMatch() == false);
         }
