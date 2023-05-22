52b6891ecf57ef277b93b8de8d3b1339af96c2bc
roniz <roniz@gingersoftware.com>
2023-05-22 10:14:23 +0300

    new WordleGame files

---
+++ Wordle/Wordle/WordleGame.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using static Wordle.WordleValidator;
+
+namespace Wordle
+{
+    public class WordleGame
+    {
+        public WordleGame(string answer, IWordValidator validator)
+        {
+
+        }
+    
+        public int TurnsRemaining()
+        {
+            return 5;
+        }
+
+        public ValidatorResult ValidateGuess(string userGuess)
+        {
+            return new ValidatorResult();
+        }
+    }
+}
---
+++ Wordle/WordleTests/WordleGameTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Wordle;
+
+namespace WordleTests
+{
+    class WordleGameTests
+    {
+        [Test]
+        public static void Constructor_DefaultInitialization_Has5TurnsRemaining()
+        {
+            // Arrange
+            string answer = "dontCare";
+
+            var mockEngDictionary = MockRepository.GenerateStub<IEnglishDictionary>();
+            mockEngDictionary.Stub(d => d.IsInDictionary("")).IgnoreArguments().Return(true);
+
+            // Act
+            var wordleGame = new WordleGame(answer, new WordleValidator(mockEngDictionary));
+
+            // Assert
+            Assert.AreEqual(5, wordleGame.TurnsRemaining());
+        }
+
+        [Test]
+        public static void ValidateGuess_UserGuessIsNot5Letters_ReturnsCorrectObject()
+        {
+            // Arrange
+            string answer = "dontCare";
+            string userGuess = "all";
+
+            var mockEngDictionary = MockRepository.GenerateStub<IEnglishDictionary>();
+            mockEngDictionary.Stub(d => d.IsInDictionary(userGuess)).Return(true);
+
+            var wordleGame = new WordleGame(answer, new WordleValidator(mockEngDictionary));
+
+            //act
+           var validateResult = wordleGame.ValidateGuess(userGuess);
+
+            // assert
+            Assert.IsFalse(validateResult.Is5Letters);
+        }
+
+    }
+}
