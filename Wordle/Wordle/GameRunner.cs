7d017f45f1b6acd59ab4ff0421ab681686851a7c
RoniZemelman <rozemelman@gmail.com>
2023-06-04 19:45:29 +0300

    GameRunner.UserIsAlive upon creation

---
+++ Wordle/Wordle/GameRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle
+{
+    public class GameRunner
+    {
+        public GameRunner(WordleGame game)
+        {
+
+        }
+    
+        public bool UserIsAlive()
+        {
+            return true;
+        }
+    }
+}
---
+++ Wordle/WordleTests/GameRunnerTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Wordle;
+using static Wordle.GuessValidator;
+
+namespace WordleTests
+{
+    class GameRunnerTests
+    {
+        [Test]
+        public static void Constructor_GameRunnerConstructed_UserIsAlive()
+        {
+            // Arrange 
+            var mockValidator = MockRepository.GenerateStub<IWordValidator>();
+            mockValidator.Stub(v => v.Validate("")).IgnoreArguments()
+                .Return(new ValidatorResult(true, true, true));
+
+            var wordleGame = new WordleGame(new GuessAnalyzer("dontCare"), mockValidator);
+
+            // Act
+            var gameRunner = new GameRunner(wordleGame);
+
+            // Assert
+            Assert.IsTrue(gameRunner.UserIsAlive());
+        }
+
+        public static void AcceptUserGuess_CorrectUserGuessEntered_IsWon()
+        {
+            //// Arrange 
+            //var mockValidator = MockRepository.GenerateStub<IWordValidator>();
+            //var mockValidator = MockRepository.GenerateStub<IWordValidator>();
+            //mockValidator.Stub(v => v.Validate("")).IgnoreArguments()
+            //    .Return(new ValidatorResult(true, true, true)); 
+            
+            //var wordleGame = new WordleGame(new GuessAnalyzer("bingo"), mockValidator);
+            //var gameRunner = new GameRunner(wordleGame);
+
+            //var enteredUserGuessDontCare = "dontCare";
+
+            //// Act
+            //gameRunner.AcceptUserGuess(enteredUserGuessDontCare);
+
+            //Assert.IsTrue(gameRunner.IsRunning());
+        }
+
+       
+
+    }
+}
