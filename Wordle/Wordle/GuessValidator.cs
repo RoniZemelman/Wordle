4262253a882c87675ec060edccfe8d54fe82281b
RoniZemelman <rozemelman@gmail.com>
2023-06-06 22:31:36 +0300

    RESTORED csproj files. NEVER GIT RESET HARD AGAIN!!!

--- Wordle/Wordle/GuessValidator.cs
+++ Wordle/Wordle/GuessValidator.cs
@@ -7,45 +7,47 @@ namespace Wordle
     {
         public class ValidatorResult 
         {
-            private readonly bool _is5Letters;
-            private readonly bool _isAllChars;
-            private readonly bool _isInDictionary;
+            private readonly bool is5Letters;
+            private readonly bool isAllChars;
+            private readonly bool isInDictionary;
             public ValidatorResult(bool is5Letters = false, 
                                    bool isAllChars = false, 
                                    bool isInDictionary = false)
             {
-                _is5Letters = is5Letters;
-                _isAllChars = isAllChars;
-                _isInDictionary = isInDictionary;
+                this.is5Letters = is5Letters;
+                this.isAllChars = isAllChars;
+                this.isInDictionary = isInDictionary;
             }
             
             public bool Is5Letters
-            { get => _is5Letters;  }
+            { get => is5Letters;  }
 
             public bool IsAllChars
-            { get => _isAllChars; }
+            { get => isAllChars; }
 
             public bool IsInDictionary
-            { get => _isInDictionary; }
+            { get => isInDictionary; }
 
             public bool IsValidGuess()
             {
-                return _is5Letters && _isAllChars && _isInDictionary;
+                return is5Letters && isAllChars && isInDictionary;
             }
         }
 
-        readonly IEnglishDictionary _dictionary; 
+        readonly IEnglishDictionary dictionary; // Non-interface?
         
         public GuessValidator(IEnglishDictionary dictionary)
         {
-            _dictionary = dictionary;
+            this.dictionary = dictionary;
         }
 
-        public ValidatorResult Validate(string guess) 
+        public ValidatorResult Validate(string guess) // returns object since did not want
+                                             // IWordValidator to be bound to WordleValidator result.
+                                             // Maybe return interface type?
         {
             return new ValidatorResult(guess.Length == 5,
                                         guess.All(character => Char.IsLetter(character)),
-                                        _dictionary.IsInDictionary(guess));
+                                        dictionary.IsInDictionary(guess));
         }
     }
 }
