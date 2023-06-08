7a91e26212db3abbdfa5a6bb38657d2b16a5aa3b
RoniZemelman <rozemelman@gmail.com>
2023-06-08 14:11:22 +0300

    Map constructed from IO words

--- Wordle/Wordle/EnglishDictionary.cs
+++ Wordle/Wordle/EnglishDictionary.cs
@@ -1,14 +1,36 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Wordle
 {
     public class EnglishDictionary : IEnglishDictionary
     {
         private StreamReader _streamReader;
+        private Dictionary<string, bool> _mapOfWords;
 
         public EnglishDictionary(string filePath)
         {
             _streamReader = new StreamReader(filePath);
+            
+            ConstructMapOfWords();
+
+            _streamReader.Close();
+        }
+
+        private void ConstructMapOfWords()
+        {
+
+            _mapOfWords = new Dictionary<string, bool>();
+            
+            string word = _streamReader.ReadLine();
+
+            while (word != null)
+            { 
+                _mapOfWords.Add(word, true);
+
+                word = _streamReader.ReadLine();
+            }
         }
 
         public bool IsInDictionary(string word)
