88dbcd293888a74ff648557616a2f34feae528b4
RoniZemelman <rozemelman@gmail.com>
2023-06-20 12:18:12 +0300

    AnswerGenerator class + tests removed, to be redone with TDD

--- Wordle/Wordle/AnswerGenerator.cs
+++ Wordle/Wordle/AnswerGenerator.cs
@@ -6,45 +6,6 @@ using System.Threading.Tasks;
 
 namespace Wordle
 {
-    public class AnswerGenerator
-    {
-        private readonly string[] _answers;
-        private readonly IWordValidator _guessValidator;
-        private readonly Random _randomizer; 
-
-
-
-        public AnswerGenerator(EnglishDictionary engDictionary, IWordValidator validator)
-        {
-            _guessValidator = validator; 
-            _answers = CreateAnswersArray(engDictionary);
-
-            _randomizer = new Random();
-        }
-
-        private string[] CreateAnswersArray(EnglishDictionary engDictionary)
-        {
-            var dictionaryWords = engDictionary.GetDictionaryWords();
-            
-            return dictionaryWords
-                .Where(word =>
-                {
-                    var validatorResult = _guessValidator.Validate(word);
-                    return validatorResult.IsValidGuess();
-                }) 
-                .ToArray();
-        }
-
-        public int NumOfAnswers()
-        {
-            return _answers.Length;
-        }
-
-        public string GenerateAnswer()
-        {
-            var randomIndex = _randomizer.Next(NumOfAnswers());
-
-            return _answers[randomIndex]; // TODO: null when empty
-        }
-    }
+    
+    
 }
--- Wordle/WordleTests2/AnswerGeneratorTests.cs
+++ Wordle/WordleTests2/AnswerGeneratorTests.cs
@@ -11,57 +11,6 @@ namespace WordleTests
 {
     class AnswerGeneratorTests
     {
-        // TODO remove filepath, configure MemoryStream in each test
-        private const string TestingDictionaryFilePath = "C:\\Users\\user\\source\\repos\\Wordle\\WordleTests2\\10000words.txt";
-        private static byte[] _fileContents;
-        private const int ExpectedNumOfValidWordleAnswers = 1379;
-
-        [OneTimeSetUp]
-        public static void ReadFileContentsIntoMainMemory()
-        {
-            _fileContents = File.ReadAllBytes(TestingDictionaryFilePath);
-        }
-
-        // TODO move the above to separate utils file - duplicated in EngDictionaryTests...
-        // TODO: use case for numOfAnswers()?
-        [Test]
-        public static void NumOfAnswers_AnswerGeneratedCreated_ExpectedNumOfAnswers()
-        {
-            // Arrange
-            var memoryStream = new MemoryStream(_fileContents);
-            var engDictionary = new EnglishDictionary(memoryStream);
-            var answerGenerator = new AnswerGenerator(engDictionary, new GuessValidator(engDictionary));
-
-            // Act
-            var numOfAnswers = answerGenerator.NumOfAnswers();
-
-            // Assert
-            Assert.AreEqual(ExpectedNumOfValidWordleAnswers, numOfAnswers);
-        }
-
-        [Test]  // TODO rewrite, mock validator as param, 1 true, all false..
-        public static void GenerateAnswer_MethodCalledForNumOfAnswers_EachAnswerIsValid()
-        {
-            // Arrange
-            var memoryStream = new MemoryStream(_fileContents);
-            var engDictionary = new EnglishDictionary(memoryStream);
-            var wordleValidator = new GuessValidator(engDictionary);
-            var answerGenerator = new AnswerGenerator(engDictionary, wordleValidator);
-
-            // Act 
-            var answerIsValid = true;
-            for (int count = 0;
-                 answerIsValid && (count < answerGenerator.NumOfAnswers()); 
-                 ++count)
-            {
-                var answer = answerGenerator.GenerateAnswer();
-                var validationResult = wordleValidator.Validate(answer);
-                answerIsValid = validationResult.IsValidGuess();
-            }
-
-            // Assert
-            Assert.IsTrue(answerIsValid);
-        }
-
+        
     }
 }
